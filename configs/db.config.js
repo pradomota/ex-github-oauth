9017b7d4973a6b950f92b430b03ebcf120a0e55e
Carlos del Prado <carlos.delprado@11paths.com>
2020-01-22 15:18:10 +0000

    chore: fix audit

--- configs/db.config.js
+++ configs/db.config.js
@@ -3,7 +3,7 @@ const DB_NAME = 'ex-github-oauth';
 const MONGO_URI = process.env.MONGO_URI || `mongodb://localhost/${DB_NAME}`;
 
 mongoose.Promise = Promise;
-mongoose.connect(MONGO_URI)
+mongoose.connect(MONGO_URI, { useNewUrlParser: true })
     .then(() => {
         console.log(`Connected to ${DB_NAME} database.`);
     }).catch((error) => {
