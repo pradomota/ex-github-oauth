62b3bae0e636f24322781fa70c818f015d45219b
Carlos del Prado <carlos.delpradomota@telefonica.com>
2021-12-22 18:10:38 +0000

    chore: fix audit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-require('dotenv').config()
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,9 +4,7 @@ const auth = require('../controllers/auth.controller');
 const passport = require('passport');
 
 router.get('/login', auth.login);
-router.post('/auth/github', 
-    passport.authenticate('github-auth', 
-        { scope: ['user:email'] }));
+router.post('/auth/github', passport.authenticate('github-auth', { scope: ['user:email'] }));
 router.get('/auth/github/cb', auth.githubAuthCb);
 router.get('/logout', auth.logout);
 
